1061637fa5f4cecd8a34bc179060dc807614e8d9
91093567 <91093567@mvusd.net>
2022-09-26 12:40:31 -0700

    Initial commit

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void displayPlayerTurn (int playerTurn) {
+        if (playerTurn == 1)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
 }
