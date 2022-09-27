5ff352e0f5267ba9fcd803977feaa53d6b55ab3e
91093567 <91093567@mvusd.net>
2022-09-27 12:04:18 -0700

    9/27/22: 12:02pm - Created method that outputs a log message when a button is pressed.  Will make it so the button will output the player turn to the log and the player title will be highlighted.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,17 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button proof = (Button)findViewById(R.id.conceptProofButton);
+
+        proof.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerDisplay();
+            }
+        }); // listener that calls next activity method
+
     }
 
-    public void displayPlayerTurn (int playerTurn) {
-        if (playerTurn == 1)
-        {
+    private void playerDisplay ()
+    {
+        Button proofOfConcept = (Button) findViewById(R.id.conceptProofButton);
+        proofOfConcept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v)
+            {
+                Log.i("info", "Player _'s turn");
+            } // end onCLick
+        });
+    } // end next button handler
 
-        }
-        else
-        {
-
-        }
-    }
 
 }
