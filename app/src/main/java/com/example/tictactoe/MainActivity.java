9f446d0c91506ef09e887586957adab82f333285
91093567 <91093567@mvusd.net>
2022-09-28 12:06:58 -0700

    9/28/22: 12:05pm - Finished code that switches players and displays player turn!

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -11,26 +11,26 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView p1Text = (TextView)findViewById(R.id.player1Text);
-    TextView p2Text = (TextView)findViewById(R.id.player2Text);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView p1Text = (TextView)findViewById(R.id.player1Text);
+        TextView p2Text = (TextView)findViewById(R.id.player2Text);
 
         Button proof = (Button)findViewById(R.id.conceptProofButton);
 
         proof.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                playerDisplay();
+                playerDisplay(p1Text, p2Text);
             }
         }); // listener that calls next activity method
 
     }
 
-    public void playerDisplay ()
+    public void playerDisplay (TextView player1Text, TextView player2Text)
     {
         Button proofOfConcept = (Button) findViewById(R.id.conceptProofButton);
         proofOfConcept.setOnClickListener(new View.OnClickListener() {
@@ -41,13 +41,13 @@ public class MainActivity extends AppCompatActivity {
                 if (pTurn % 2 == 0)
                 {
                     Log.i("info", "Player " + 2 + "'s turn");
-                    //p2Text.setBackgroundColor(Color.RED);
-                    //p1Text.setBackgroundColor(Color.WHITE);
+                    player1Text.setBackgroundColor(Color.WHITE);
+                    player2Text.setBackgroundColor(Color.YELLOW);
                 }
                 else {
                     Log.i("info", "Player " + 1 + "'s turn");
-                    //p1Text.setBackgroundColor(Color.RED);
-                    //p2Text.setBackgroundColor(Color.WHITE);
+                    player2Text.setBackgroundColor(Color.WHITE);
+                    player1Text.setBackgroundColor(Color.YELLOW);
                 }
                 pTurn++;
             } // end onCLick
