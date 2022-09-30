494aa69a9481d6ee70bfd5e472199bdaece18a89
Jin <jindebest@gmail.com>
2022-09-30 12:11:18 -0700

    started prroof of concept

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -5,24 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.tictactoe.R;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private int player = 1;
+    Button btn;
+    TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener()
-        {
+        btn = (Button) findViewById(R.id.btn);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                String newText = textView2.getText
+            public void onClick(View view) {
+                player = switchPlayer(player);
+                textView2.setText(Integer.toString(player));
             }
         });
     }
-
     public static int switchPlayer(int player)
     {
         if(player == 1)
@@ -32,5 +36,3 @@ public class MainActivity extends AppCompatActivity {
         return player;
     }//end methodxde
 }
-
-
